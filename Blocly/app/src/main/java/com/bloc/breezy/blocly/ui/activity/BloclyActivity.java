a487096f4e579bfdae0fe09a7a42f61733d8ec55
Christian Ambriz <christian.ambriz@bloc.io>
2015-11-21 19:21:34 -0800

    checkpoint-33-work

---
+++ Blocly/app/src/main/java/com/bloc/breezy/blocly/ui/activity/BloclyActivity.java
@@ -0,0 +1,18 @@
+package com.bloc.breezy.blocly.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.bloc.breezy.blocly.R;
+
+
+/**
+ * Created by cambriz on 11/21/15.
+ */
+public class BloclyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
+}
