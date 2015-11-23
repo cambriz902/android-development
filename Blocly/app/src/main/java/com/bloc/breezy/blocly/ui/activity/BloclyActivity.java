2a7b7f70ae1b2d7cd06b7371d47fdffbadcb2700
Christian Ambriz <christian.ambriz@bloc.io>
2015-11-22 16:47:48 -0800

    checkpoint 34 assignment complete

--- Blocly/app/src/main/java/com/bloc/breezy/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/com/bloc/breezy/blocly/ui/activity/BloclyActivity.java
@@ -2,7 +2,7 @@ package com.bloc.breezy.blocly.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.bloc.breezy.blocly.BloclyApplication;
 import com.bloc.breezy.blocly.R;
@@ -12,12 +12,25 @@ import com.bloc.breezy.blocly.R;
  * Created by cambriz on 11/21/15.
  */
 public class BloclyActivity extends Activity {
+
+    private TextView centertext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
-        Toast.makeText(this,
-                BloclyApplication.getSharedDataSource().getFeeds().get(0).getTitle(),
-                Toast.LENGTH_LONG).show();
+        String firstFeedTitle = BloclyApplication
+                .getSharedDataSource()
+                .getFeeds()
+                .get(0)
+                .getTitle();
+        setCentertext(firstFeedTitle);
     }
+
+    // Private methods
+    private void setCentertext(String text){
+        centertext = (TextView) findViewById(R.id.centertext);
+        centertext.setText(text);
+    }
+
 }
