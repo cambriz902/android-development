c29568db0d254174217bb1912a60c97367c95ea9
Christian Ambriz <christian.ambriz@bloc.io>
2015-11-27 12:10:29 -0800

    checkpoint 38 completed

--- Blocly/app/src/main/java/com/bloc/breezy/blocly/BloclyApplication.java
+++ Blocly/app/src/main/java/com/bloc/breezy/blocly/BloclyApplication.java
@@ -3,18 +3,21 @@ package com.bloc.breezy.blocly;
 import android.app.Application;
 
 import com.bloc.breezy.blocly.api.DataSource;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 /**
  * Created by cambriz on 11/22/15.
  */
 public class BloclyApplication extends Application {
 
-    // #1
     public static BloclyApplication getSharedInstance() {
         return sharedInstance;
     }
 
-    // #2
     public static DataSource getSharedDataSource() {
         return BloclyApplication.getSharedInstance().getDataSource();
     }
@@ -22,12 +25,32 @@ public class BloclyApplication extends Application {
     private static BloclyApplication sharedInstance;
     private DataSource dataSource;
 
-    // #3
     @Override
     public void onCreate() {
         super.onCreate();
         sharedInstance = this;
         dataSource = new DataSource();
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions
+                .Builder()
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .build();
+
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration
+                .Builder(this)
+                .tasksProcessingOrder(QueueProcessingType.FIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+
+        ImageLoader.getInstance().init(configuration);
+
+
     }
 
     public DataSource getDataSource() {
