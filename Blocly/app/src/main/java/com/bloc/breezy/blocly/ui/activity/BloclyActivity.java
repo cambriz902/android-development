2b4c610f9b4734188f4c5e268e102e83aca6ea17
Christian Ambriz <christian.ambriz@bloc.io>
2015-11-30 09:25:49 -0800

    checkpoint 38 assignment complete

--- Blocly/app/src/main/java/com/bloc/breezy/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/com/bloc/breezy/blocly/ui/activity/BloclyActivity.java
@@ -1,13 +1,19 @@
 package com.bloc.breezy.blocly.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.bloc.breezy.blocly.R;
 import com.bloc.breezy.blocly.ui.adapter.ItemAdapter;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 
 /**
@@ -16,6 +22,7 @@ import com.bloc.breezy.blocly.ui.adapter.ItemAdapter;
 public class BloclyActivity extends Activity {
 
     private ItemAdapter itemAdapter;
+    private ImageView background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,36 @@ public class BloclyActivity extends Activity {
         itemAdapter = new ItemAdapter();
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
-        // #12
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
+
+
+        background = (ImageView) findViewById(R.id.background);
+        ImageLoader.getInstance().loadImage("http://crackberry.com/sites/crackberry.com/files/styles" +
+                "/large/public/topic_images/2013/ANDROID.png?itok=xhm7jaxS", new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                if(background != null) {
+                    background.setImageBitmap(loadedImage);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+            }
+        });
+
     }
 }
